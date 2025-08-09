0ed98bf717ae94c96f3f99cc21cf273513ee1e6d
taryaalo <tarya.alorofie@gmail.com>
2025-08-09 13:55:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 // Generate a simple session id and store it in localStorage
 const sidKey = 'sessionId';
